c071bdcef0714ae6cea51efef5b8ecdc1485f530
rabo <rabobahago@gmail.com>
2021-05-22 17:39:03 +0100

    first component added

---
+++ src/components/counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {
+    count: 2,
+  };
+  formatCount = () => {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  };
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  render() {
+    return (
+      <>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">increment</button>
+      </>
+    );
+  }
+}
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,47 +1,5 @@
-const object = {
-  name: "rabo",
-  walk() {
-    console.log(this);
-  },
-};
-object.walk();
+import ReactDOM from "react-dom";
+import Counter from "./components/counter";
+import "bootstrap/dist/css/bootstrap.css";
 
-let result = object.walk.bind(object);
-result();
-
-const person = {
-  name: "Bolagi",
-  wailk: function () {
-    console.log(this);
-  },
-};
-console.log(person);
-
-class MajorColor {
-  constructor(name) {
-    this.name = name;
-  }
-  whatColor() {
-    console.log("Hello I am ", this.name);
-  }
-}
-
-class SpecialColor extends MajorColor {
-  constructor(name, age) {
-    super(name);
-    this.age = age;
-  }
-  whatColorDo() {
-    console.log("I am a color the child class " + this.name);
-  }
-}
-
-const majorColorRed = new MajorColor("red");
-const majorColorBlue = new MajorColor("blue");
-
-console.log(majorColorRed);
-console.log(majorColorBlue);
-console.log(majorColorRed.whatColor());
-
-const specialColor = new SpecialColor("#000000", 23);
-console.log(specialColor.whatColor());
+ReactDOM.render(<Counter />, document.querySelector("#root"));
