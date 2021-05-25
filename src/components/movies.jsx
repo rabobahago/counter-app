24ba15ab30191498b7aa7a2b76c04f77e7f27e76
rabo <rabobahago@gmail.com>
2021-05-25 20:22:16 +0100

    pagination completed

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
 import Pagination from "./common/pagination";
+import { paginate } from "./utils/paginate";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
     currentPage: 1,
-    pageSize: 2,
+    pageSize: 5,
   };
   handleDelete = (movie) => {
     let movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -25,56 +26,55 @@ class Movies extends Component {
   };
   render() {
     const { length: count } = this.state.movies;
-    let data =
-      count === 0 ? (
-        <p>There are no movies in the database</p>
-      ) : (
-        <React.Fragment>
-          <p>Showing {count} movies in the database</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
-                <th></th>
+    const { pageSize, currentPage, movies } = this.state;
+    if (count === 0) return <p>There are no movies in the database</p>;
+    const allMovies = paginate(movies, currentPage, pageSize);
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allMovies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.rate}</td>
+                <td>
+                  <Like
+                    onClick={() => this.handleClick(movie)}
+                    liked={movie.liked}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    delete
+                  </button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {this.state.movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.rate}</td>
-                  <td>
-                    <Like
-                      onClick={() => this.handleClick(movie)}
-                      liked={movie.liked}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <Pagination
-            itemsCount={count}
-            pageSize={this.state.pageSize}
-            onPageChange={this.handlePageChange}
-            onCurrentPage={this.state.currentPage}
-          />
-        </React.Fragment>
-      );
-    return data;
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+          onCurrentPage={currentPage}
+        />
+      </React.Fragment>
+    );
   }
 }
 export default Movies;
