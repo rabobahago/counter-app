b17ef9eb7f9934d39fb12d9a41e2319aa208f0a0
rabo <rabobahago@gmail.com>
2021-05-27 21:45:26 +0100

    refactored movies table component to table header component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -12,7 +12,7 @@ class Movies extends Component {
     movies: [],
     genres: [],
     currentPage: 1,
-    pageSize: 5,
+    pageSize: 3,
     sortColumn: { path: "title", order: "asc" },
   };
   componentDidMount() {
